3b0cbf03be97b1268b739a4e595adb66ebf187ec
Pedro Pablo <pmontesarroyave@gmail.com>
2024-05-23 22:46:25 -0500

    creamos el spia con el mock

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -6,16 +6,19 @@ const fakeBooks = [
     name: 'Book 1',
   },
 ]
-const sypGetAll = jest.fn()
+const mockSypGetAll = jest.fn()
 
 const MongoLibSub = {
-  getAll: sypGetAll,
+  getAll: mockSypGetAll,
   create: () => { },
 }
 
 
 jest.mock('../lib/mongo.lib', () => {
-  return jest.fn().mockImplementation(() => MongoLibSub)
+  return jest.fn().mockImplementation(() => ({
+    getAll: mockSypGetAll,
+    create: () => { },
+  }))
 })
 
 describe('test for BooksService', () => {
@@ -29,23 +32,25 @@ describe('test for BooksService', () => {
   describe('test for getBooks method', () => {
     it('should return an array of books', async () => {
       // arrange
-
+      mockSypGetAll.mockResolvedValue(fakeBooks);
+      const query = {};
       // act
-      const books = await service.getBooks({});
+      const books = await service.getBooks(query);
       // assert
       expect(books.length).toEqual(1);
     })
 
     it('debe contener el id ', async () => {
       // arrange
-      sypGetAll.mockResolvedValue(fakeBooks);
+      mockSypGetAll.mockResolvedValue(fakeBooks);
       const query = {};
       // act
       const books = await service.getBooks(query);
       // assert
-      expect(books[0]._id).toEqual('60f1b3b3b3b3b3b3b3b3b3');
-      expect(sypGetAll).toHaveBeenCalled()
-      expect(sypGetAll).toHaveBeenCalledWith('book', query)
+      expect(books[0].name).toEqual('Book 1');
+      expect(mockSypGetAll).toHaveBeenCalled()
+      expect(mockSypGetAll).toHaveBeenCalledTimes(1)
+      expect(mockSypGetAll).toHaveBeenCalledWith(service.collection, query)
     })
 
   })
